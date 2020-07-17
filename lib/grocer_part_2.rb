91f0bca338073ba390cd0df543277a58bf74befc
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-17 14:54:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -43,7 +43,7 @@ end
 def checkout(cart, coupons)
  
       consolidated_cart = consolidate_cart(cart)
-      coupons_applied = apply_coupons(consolidated_cart)
+      coupons_applied = apply_coupons(consolidated_cart, coupons)
       final_cart = apply_clearance(coupons_applied)
   total = 0 
   
