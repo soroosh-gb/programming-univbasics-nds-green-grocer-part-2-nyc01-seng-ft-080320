0061c070de7d70805308b756bd33439da2ac88ce
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-17 14:45:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,60 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
+  i = 0 
+  while i < coupons.length do 
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+      if cart_item && cart_item[:count] >= coupons[i][:num]
+        if cart_item_with_coupon
+          cart_item_with_coupon[:count] += coupons[i][:num] 
+          cart_item[:count] -= coupons[i][:num]
+        else 
+          cart_item_with_coupon = {
+            :item => couponed_item_name,
+            :price => coupons[i][:cost] / coupons[i][:num],
+            :count => coupons[i][:num],
+            :clearance => cart_item[:clearance]
+          }
+          cart << cart_item_with_coupon
+          cart_item[:count] -= coupons[i][:num]
+        end
+      end 
+    i += 1 
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0 
+  while i < cart.length do 
+    if cart[i][:clearance]
+      cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.2)).round(2)
+    end 
+    i +=1 
+  end
+  cart 
 end
 
+  
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+ 
+      consolidated_cart = consolidate_cart(cart)
+       
+      coupons_applied = apply_clearance(consolidated_cart)
+      final_cart = apply_clearance(coupons_applied)
+  total = 0 
+  
+    i = 0 
+    while i < final_cart.length do 
+      total += final_cart[i][:price] * final_cart[i][:count]
+      i += 1 
+    end
+      if total > 100
+       total = total - (total * 0.1)
+      end
+    total 
 end
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -10,25 +10,46 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 
-def consolidate_cart(cart)
-  index = 0
-  new_cart = []
+# def consolidate_cart(cart)
+#   index = 0
+#   new_cart = []
   
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
+#   cart.each do |grocery_item|
+#     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+#     if current_item
+#       new_cart_index = 0
+#       new_cart.each do |new_cart_item|
+#         if new_cart_item[:item] === current_item[:item]
+#           new_cart_item[:count] += 1
+#         end
+#         new_cart_index += 1
+#       end
+#     else
+#       grocery_item[:count] = 1
+#       new_cart << grocery_item
+#     end
+#     index += 1
+#   end
+#   new_cart
+# end
+def consolidate_cart(cart)
+  new_cart = []
+  i = 0 
+  while i < cart.length do 
+    new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart)
+    if new_cart_item != nil 
+      new_cart_item[:count] += 1 
+    else 
+      new_cart_item = {
+      :item => cart[i][:item],
+      :price => cart[i][:price],
+      :clearance => cart[i][:clearance],
+      :count => 1 
+    }
+      new_cart << new_cart_item
+    end 
+    i +=1 
     end
-    index += 1
-  end
   new_cart
+  
 end
